42d954d5ad6b9c7c35ef2a2922488e4be467a86b
Jose Troche <jose.troche.coder@gmail.com>
2025-03-27 11:41:58 -0400

    removed original app.js and yarn.lock

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
